ab252a7696c757ab4bc3b506a2c111ded5359412
WNBA DASHBOARD <wnbadashboard@gmail.com>
2026-07-15 03:38:24 -0700

    Create playerDatabase.js

---
+++ js/playerDatabase.js
@@ -0,0 +1,134 @@
+// ATHENA PLAYER DATABASE
+// Sprint 18
+
+export const playerDatabase = {
+
+    default:{
+
+        minutes:30,
+
+        fppm:1.00
+
+    },
+
+    "A'ja Wilson":{
+
+        minutes:35,
+
+        fppm:1.48
+
+    },
+
+    "Breanna Stewart":{
+
+        minutes:34,
+
+        fppm:1.37
+
+    },
+
+    "Napheesa Collier":{
+
+        minutes:36,
+
+        fppm:1.32
+
+    },
+
+    "Sabrina Ionescu":{
+
+        minutes:34,
+
+        fppm:1.12
+
+    },
+
+    "Jonquel Jones":{
+
+        minutes:31,
+
+        fppm:1.25
+
+    },
+
+    "Jackie Young":{
+
+        minutes:35,
+
+        fppm:1.08
+
+    },
+
+    "Kelsey Plum":{
+
+        minutes:34,
+
+        fppm:1.03
+
+    },
+
+    "Chelsea Gray":{
+
+        minutes:31,
+
+        fppm:1.01
+
+    },
+
+    "Arike Ogunbowale":{
+
+        minutes:36,
+
+        fppm:1.14
+
+    },
+
+    "Caitlin Clark":{
+
+        minutes:35,
+
+        fppm:1.18
+
+    },
+
+    "Aliyah Boston":{
+
+        minutes:32,
+
+        fppm:1.08
+
+    },
+
+    "Rhyne Howard":{
+
+        minutes:35,
+
+        fppm:1.05
+
+    },
+
+    "Allisha Gray":{
+
+        minutes:34,
+
+        fppm:1.03
+
+    },
+
+    "Angel Reese":{
+
+        minutes:34,
+
+        fppm:1.10
+
+    },
+
+    "Kamilla Cardoso":{
+
+        minutes:30,
+
+        fppm:1.02
+
+    }
+
+};
